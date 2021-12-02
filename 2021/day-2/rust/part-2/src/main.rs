d7e213011da2bbadf5a084b3237a2a47ac86b98d
cskwrd <cskwrd@users.noreply.github.com>
2021-12-02 17:45:54 -0600

    Add day 2, part 2, rust solution.

--- 2021/day-2/rust/part-2/src/main.rs
+++ 2021/day-2/rust/part-2/src/main.rs
@@ -1,28 +1,32 @@
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    let filename = &args[1];
+    let filename = std::env::args().nth(1).unwrap(); // find a safer way to handle Option<>
 
     let contents = std::fs::read_to_string(filename).expect("Something went wrong reading the file");
 
     let mut position = 0;
     let mut depth =  0;
+    let mut aim = 0;
     for line in contents.lines() {
         let mut instruction = line.split_ascii_whitespace();
         
         match instruction.next() {
             Some("forward") => {
                 if let Some(magnitude) = instruction.next() {
-                    position = match magnitude.parse::<i32>() {
-                        Ok(value) => position + value,
+                    match magnitude.parse::<i32>() {
+                        Ok(value) => {
+                            position += value;
+                            depth += aim * value;
+                        },
                         Err(_) => break, // should really handle this case some how so we don't print a partial answer, try/catch maybe?
                     };
+                    
                 }
                 continue;
             },
             Some("down") => {
                 if let Some(delta) = instruction.next() {
-                    depth = match delta.parse::<i32>() {
-                        Ok(value) => depth + value, // i wonder if there is a way to merge this match with the "up" match and just change the operation, like a case statement without a break in C#
+                    aim = match delta.parse::<i32>() {
+                        Ok(value) => aim + value, // i wonder if there is a way to merge this match with the "up" match and just change the operation, like a case statement without a break in C#
                         Err(_) => break,
                     };
                 }
@@ -30,8 +34,8 @@ fn main() {
             },
             Some("up") => {
                 if let Some(delta) = instruction.next() {
-                    depth = match delta.parse::<i32>() {
-                        Ok(value) => depth - value,
+                    aim = match delta.parse::<i32>() {
+                        Ok(value) => aim - value,
                         Err(_) => break,
                     };
                 }
