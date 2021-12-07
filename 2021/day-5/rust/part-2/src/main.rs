bb57321c11f63c85cb1175b9da999ff3c8250702
cskwrd <cskwrd@users.noreply.github.com>
2021-12-06 22:17:31 -0600

    Add incomplete solution for day 5, part 2.

--- 2021/day-5/rust/part-2/src/main.rs
+++ 2021/day-5/rust/part-2/src/main.rs
@@ -1,73 +1,44 @@
 use core::fmt;
-use std::{fmt::Display, collections::HashMap};
+use std::{fmt::Display, collections::HashMap, collections::HashSet};
+
+use num::Integer;
 
 fn main() {
     println!("\nbeginning hydrothermal analysis...");
     
     let filename = std::env::args().nth(1).unwrap(); // could make this safer using `if let`
     let contents = std::fs::read_to_string(filename).expect("Something went wrong reading the file");
-    let lines_in_file = contents.lines();
+    let endpoints_in_file = contents.lines()
+        .map(|l| {
+            let (start, end) = l.split_once(" -> ").unwrap();
+            let (start_x, start_y) = start.trim().split_once(',').unwrap();
+            let (end_x, end_y) = end.trim().split_once(',').unwrap();
+            LineSegment::new(Point { x: start_x.parse().unwrap(), y: start_y.parse::<i32>().unwrap() }, Point { x: end_x.parse().unwrap(), y: end_y.parse::<i32>().unwrap() })
+        })
+        .filter(|ls| {
+            is_horizontal(ls) || is_vertical(ls) || is_diagonal(ls)
+        });
 
-    let mut sea_floor: HashMap<Point, i32> = HashMap::new();
-    let mut num_points_of_interest = 0;
-    for reading in lines_in_file {
-        if let Some((segment_start, segment_end)) = reading.split_once(" -> ") {
-            let start_point = parse_coordinates(segment_start).expect("unable to parse segment start");
-            let end_point = parse_coordinates(segment_end).expect("unable to parse segment end");
-            if interesting_segment(&start_point, &end_point) {
-                let x: Point = make_positive_point(start_point.x, end_point.x);
-                let y: Point = make_positive_point(start_point.y, end_point.y);
-                for i in x.x..=x.y {
-                    for j in y.x..=y.y {
-                        let point_of_interest = Point { x: i, y: j };
-
-                        // the following line throws a warning, but trying to fix it makes the contained code throw an error
-                        // comparing to the example at https://doc.rust-lang.org/std/collections/hash_map/enum.Entry.html#method.or_insert
-                        // i think the problem comes from the `let` part
-                        // if let point_of_interest_magnitude = sea_floor.entry(point_of_interest).or_insert(0) {
-                        //     *point_of_interest_magnitude += 1;
-                        //     if *point_of_interest_magnitude == 2 {
-                        //         num_points_of_interest += 1;
-                        //     }
-                        // }
-
-                        if let Some(point_of_interest_magnitude) = sea_floor.get_mut(&point_of_interest) {
-                            *point_of_interest_magnitude += 1;
-                            if *point_of_interest_magnitude == 2 {
-                                // only count the point of interest once
-                                num_points_of_interest += 1;
-                            }
-                        } else {
-                            sea_floor.insert(point_of_interest, 1);
-                        }
-                    }
-                }
-            }
-        }
+    for line_segment in endpoints_in_file {
+        println!("{}", line_segment);
     }
 
-    println!("\nfound {} points of interest!!\n\nending hydrothermal analysis...\n", num_points_of_interest);
+    // println!("\nfound {:?} points of interest!!\n\nending hydrothermal analysis...\n", num_points_of_interest);
 }
 
-fn make_positive_point(first_coordinate: i32, second_coordinate: i32) -> Point {
-    match first_coordinate <= second_coordinate {
-        true => Point { x: first_coordinate, y: second_coordinate },
-        false => Point { x: second_coordinate, y: first_coordinate },
-    }
+fn is_horizontal(line_segment: &LineSegment) -> bool { // &'s are needed so we don't take ownership from caller
+    line_segment.start.x == line_segment.end.x
 }
 
-fn interesting_segment(start: &Point, end: &Point) -> bool { // &'s are needed so we don't take ownership from caller
-    start.x == end.x || start.y == end.y
+fn is_vertical(line_segment: &LineSegment) -> bool { // &'s are needed so we don't take ownership from caller
+    line_segment.start.y == line_segment.end.y
 }
 
-fn parse_coordinates(coordinates: &str) -> Option<Point> {
-    if let Some((x_coordinate, y_coordinate)) = coordinates.split_once(',') {
-        return Some(Point { x: x_coordinate.parse().expect("unable to parse x coordinate"), y: y_coordinate.parse().expect("unable to parse y coordinate") });
-    }
-    None
+fn is_diagonal(line_segment: &LineSegment) -> bool { // &'s are needed so we don't take ownership from caller
+    line_segment.slope.abs() == 1
 }
 
-#[derive(PartialEq, Eq, Hash)] // this appears to "auto-implement" these functions or traits as they're called
+#[derive(Clone, Debug, PartialEq, Eq, Hash)] // this appears to "auto-implement" these functions or traits as they're called
 struct Point {
     x: i32,
     y: i32,
@@ -76,5 +47,43 @@ struct Point {
 impl Display for Point {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "Point {{ x: {}, y: {} }}", self.x, self.y)
+    }
+}
+
+struct LineSegment {
+    start: Point,
+    end: Point,
+    rise: i32,
+    run: i32,
+    slope: i32,
+    b: i32,
+}
+
+impl LineSegment {
+    fn new(start: Point, end: Point) -> LineSegment {
+        let mut rise = end.y - start.y;
+        let mut run = end.x - start.x;
+
+        let gcd = rise.gcd(&run);
+
+        rise /= gcd;
+        run /= gcd;
+
+        let slope: i32;
+        if run != 0 {
+            slope = rise / run;
+        } else {
+            slope = 0;
+        }
+
+        let b = start.y - (slope * start.x);
+
+        LineSegment { start, end, rise, run, slope, b }
+    }
+}
+
+impl Display for LineSegment {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "LineSegment {{ ({},{}) -> ({},{}) }}", self.start.x, self.start.y, self.end.x, self.end.y)
     }
 }
