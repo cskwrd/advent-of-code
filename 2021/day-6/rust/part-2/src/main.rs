9ee10a784b69c9999f4c09221463eafd1553a114
cskwrd <cskwrd@users.noreply.github.com>
2021-12-11 09:15:45 -0600

    Attempt to track fish groups instead of all fish.

--- 2021/day-6/rust/part-2/src/main.rs
+++ 2021/day-6/rust/part-2/src/main.rs
@@ -1,14 +1,31 @@
-use std::{collections::LinkedList, fmt::{Display, self, Debug}};
+use std::{collections::{LinkedList}, fmt::{Display, self, Debug}};
 
 fn main() {
-    let filename = std::env::args().nth(1).unwrap(); // could make this safer using `if let`
-    let contents = std::fs::read_to_string(filename).expect("Something went wrong reading the file");
+    let args: Vec<String> = std::env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Error occurred: {}", err);
+        std::process::exit(1);
+    });
+
+    let contents = std::fs::read_to_string(config.input_file).expect("Something went wrong reading the file");
     let input = contents.lines().next().unwrap();
 
     let mut school_of_fish: LinkedList<LanternFish> = LinkedList::new();
+    // let mut something_else: HashMap<i32, LanternFish> = HashMap::new();
+    // for fish_timer in input.split(',') {
+    //     let new_fish = LanternFish::new(fish_timer.parse().unwrap(), 0);
+    //     if let Some(existing_fish) = something_else.get_mut(&new_fish.timer) {
+    //         (*existing_fish).mutations += 1;
+    //     } else {
+    //         something_else.insert(new_fish.timer, new_fish);
+    //         school_of_fish.push_back(new_fish.clone());
+    //     }
+    // }
     for fish_timer in input.split(',') {
-        school_of_fish.push_back(LanternFish::new(fish_timer.parse().unwrap()))
+        school_of_fish.push_back(LanternFish::new(fish_timer.parse().unwrap(), 0))
     }
+
+    // println!("School: {:?}", school_of_fish);
 
     if input.len() <= 10 {
         println!("Initial state: [{}]", input);
@@ -16,7 +33,7 @@ fn main() {
         println!("Initial state: [{} fish]", school_of_fish.len());
     }
     
-    let days = std::env::args().nth(2).unwrap().parse::<i32>().unwrap();
+    let days = config.days;
     let mut baby_sharks: LinkedList<LanternFish> = LinkedList::new();
     for i in 0..days {
         for fish in school_of_fish.iter_mut() {
@@ -31,13 +48,15 @@ fn main() {
     println!("{} fish are in class", school_of_fish.len());
 }
 
+#[derive(Clone, Copy)]
 struct LanternFish {
     timer: i32,
+    mutations: i32,
 }
 
 impl LanternFish {
-    fn new(timer: i32) -> LanternFish {
-        LanternFish { timer }
+    fn new(timer: i32, mutations: i32) -> LanternFish {
+        LanternFish { timer, mutations }
     }
 
     fn advance_age(&mut self) -> Option<LanternFish> {
@@ -45,7 +64,7 @@ impl LanternFish {
         match self.timer {
             -1 => {
                 self.timer = 6;
-                Some(LanternFish::new(8))
+                Some(LanternFish::new(8, 0))
             },
             _ => None,
         }
@@ -60,6 +79,26 @@ impl Display for LanternFish {
 
 impl Debug for LanternFish {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.timer)
+        write!(f, "{}@{}", self.mutations + 1, self.timer)
+    }
+}
+
+struct Config {
+    input_file: String,
+    days: i32,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("need more args");
+        }
+
+        let input_file = args[1].clone();
+        let days_str = args[2].clone();
+
+        let days: i32 = days_str.to_string().parse().unwrap();
+
+        Ok(Config { input_file, days })
     }
 }
