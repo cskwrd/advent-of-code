de775932de20761cce8c7232bcebef89efc9dc99
cskwrd <cskwrd@users.noreply.github.com>
2021-12-13 15:42:26 -0600

    Add day 7, part 2, solution.

--- 2021/day-7/rust/part-2/src/main.rs
+++ 2021/day-7/rust/part-2/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
@@ -6,13 +8,42 @@ fn main() {
     });
 
     let contents = std::fs::read_to_string(config.input_file).expect("Something went wrong reading the file");
-    let mut crab_positions = parse_input(contents.lines());
-    crab_positions.sort();
-    let middle_crab = crab_positions[crab_positions.len() / 2];
+    let crab_positions = parse_input(contents.lines());
+    
+    let average_crab_position = (crab_positions.iter().sum::<i32>() as f32 / crab_positions.len() as f32).round() as i32;
+    dbg!(average_crab_position);
 
-    let min_fuel: i32 = crab_positions.iter().map(|p| i32::abs(middle_crab - p)).sum();
+    let mut lowest_fuel_consumption = calculate_fuel_consumption(crab_positions.as_slice(), average_crab_position);
+    let mut target_position = average_crab_position;
+    let mut consumption_calculations: HashMap<i32, i32> = HashMap::new(); // store calculations so we don't need to recalc them over and over.
+    consumption_calculations.insert(average_crab_position, lowest_fuel_consumption);
+    let min_fuel: i32 = loop {
+        let target_position_plus_one = target_position + 1;
+        let target_position_minus_one = target_position - 1;
+
+        let fuel_consumption_approaching_positive_inf = *consumption_calculations.entry(target_position_plus_one).or_insert(calculate_fuel_consumption(crab_positions.as_slice(), target_position_plus_one));
+        let fuel_consumption_approaching_negative_inf = *consumption_calculations.entry(target_position_minus_one).or_insert(calculate_fuel_consumption(crab_positions.as_slice(), target_position_minus_one));
+
+        if fuel_consumption_approaching_positive_inf < lowest_fuel_consumption {
+            lowest_fuel_consumption = fuel_consumption_approaching_positive_inf;
+            target_position = target_position_plus_one;
+        } else if fuel_consumption_approaching_negative_inf < lowest_fuel_consumption {
+            lowest_fuel_consumption = fuel_consumption_approaching_negative_inf;
+            target_position = target_position_minus_one;
+        } else {
+            break lowest_fuel_consumption;
+        }
+    };
 
     println!("Min fuel: {}", min_fuel);
+}
+
+fn calculate_fuel_consumption(positions: &[i32], target_position: i32) -> i32 {
+    positions.iter().map(|p| sorta_factorial(i32::abs(target_position - p))).sum()
+}
+
+fn sorta_factorial(n: i32) -> i32 {
+    (1..=n).sum()
 }
 
 fn parse_input(input: std::str::Lines) -> Vec<i32> {
