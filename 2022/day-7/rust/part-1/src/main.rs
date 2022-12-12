6e69c4b2d828022e8c24b9d9bf1e484be1135d3b
cskwrd <cskwrd@users.noreply.github.com>
2022-12-12 11:35:41 -0500

    Add parsing crate
    nom is a parsing crate used to make, well... parsers.
    It seems to be fast
    and makes it easy to work with the text from the input files.

--- 2022/day-7/rust/part-1/src/main.rs
+++ 2022/day-7/rust/part-1/src/main.rs
@@ -1,13 +1,82 @@
 use std::{collections::HashMap, path::{Path, PathBuf}};
 
+use nom::{branch::alt, bytes::complete::{take_while1, tag}, combinator::{map, all_consuming}, Finish, IResult, sequence::{preceded, separated_pair}};
+
+#[derive(Debug)]
+struct Ls;
+
+fn parse_ls(i: &str) -> IResult<&str, Ls> {
+    map(tag("ls"), |_| Ls)(i)
+}
+
+#[derive(Debug)]
+struct Cd(PathBuf);
+
+fn parse_cd(i: &str) -> IResult<&str, Cd> {
+    map(preceded(tag("cd "), parse_path), Cd)(i)
+}
+
+#[derive(Debug)]
+enum Command {
+    Ls(Ls),
+    Cd(Cd),
+}
+
+fn parse_command(i: &str) -> IResult<&str, Command> {
+    let (i, _) = tag("$ ")(i)?;
+    alt((map(parse_ls, Command::Ls), map(parse_cd, Command::Cd)))(i)
+}
+
+#[derive(Debug)]
+enum Entry {
+    Dir(PathBuf),
+    File(u64, PathBuf),
+}
+
+fn parse_path(path: &str) -> IResult<&str, PathBuf> {
+    map(
+        take_while1(|c| "abcdefghijklmnopqrstuvwxyz./".contains(c)),
+        Into::into
+    )(path)
+}
+
+fn parse_entry(i: &str) -> IResult<&str, Entry> {
+    let parse_file = map(
+        separated_pair(nom::character::complete::u64, tag(" "), parse_path),
+        |(size, path)| Entry::File(size, path),
+    );
+    let parse_dir = map(preceded(tag("dir "), parse_path), Entry::Dir);
+
+    alt((parse_file, parse_dir))(i)
+}
+
+#[derive(Debug)]
+enum Line {
+    Command(Command),
+    Entry(Entry),
+}
+
+fn parse_line(i: &str) -> IResult<&str, Line> {
+    alt((
+        map(parse_command, Line::Command),
+        map(parse_entry, Line::Entry),
+    ))(i)
+}
+
 fn main() -> anyhow::Result<()> {
     // it is my understanding that include_str "opens" the file at compile time
     // this means that we check the file exists at compile time and don't need to handle an error here
     // it also means that the file's contents is compiled into the resulting binary
     // normally that wouldn't be ideal
-    let input_lines = include_str!("../../../aoc-input.txt").lines();
+    let input_lines = include_str!("../../../aoc-input.txt")
+        .lines()
+        .map(|l| all_consuming(parse_line)(l).finish().unwrap().1);
 
-    println!("dir size: '{}'", solve(input_lines, 100000));
+    for line in input_lines {
+        println!("{line:?}");
+    }
+
+    //println!("dir size: '{}'", solve(input_lines, 100000));
     
     Ok(())
 }
