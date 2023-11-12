b63d6c107a0660c7d74ad884ba96e697572ead73
cskwrd <cskwrd@users.noreply.github.com>
2023-11-11 22:16:38 -0600

    Solve Day 2 Part 2

--- src/AOC/TwentyTwo/Day2.cs
+++ src/AOC/TwentyTwo/Day2.cs
@@ -4,9 +4,9 @@ using System.Runtime.CompilerServices;
 namespace AOC.TwentyTwo;
 public class Day2 : DayBase
 {
-    private static readonly List<long> _emptyParsedPocketContents = new List<long> { 0 };
+    public override string AnswerPart1(string input) => DecryptStrategyGuideViaDirectResponse(input).Select(decryptedPage => CalculateScoreForRound(decryptedPage.call, decryptedPage.response)).Sum().ToString();
 
-    public override string AnswerPart1(string input) => DecryptStrategyGuide(input).Select(decryptedPage => CalculateScoreForRound(decryptedPage.call, decryptedPage.response)).Sum().ToString();
+    public override string AnswerPart2(string input) => DecryptStrategyGuideViaCalculatedResponse(input).Select(decryptedPage => CalculateScoreForRound(decryptedPage.call, decryptedPage.response)).Sum().ToString();
 
     private static int CalculateScoreForRound(Moves call, Moves response)
     {
@@ -58,9 +58,7 @@ public class Day2 : DayBase
         throw new Exception($"Problem tallying score for: '{call}' x '{response}'");
     }
 
-    public override string AnswerPart2(string input) => throw new NotImplementedException();
-
-    private static IEnumerable<(Moves call, Moves response)> DecryptStrategyGuide(string strategyGuide)
+    private static IEnumerable<(Moves call, Moves response)> DecryptStrategyGuideViaDirectResponse(string strategyGuide)
     {
         if (string.IsNullOrWhiteSpace(strategyGuide) == false)
         {
@@ -85,6 +83,89 @@ public class Day2 : DayBase
                     _ => throw new Exception($"Unable to decrypt response: '{encryptedResponse}'"),
                 };
                 yield return (call: decryptedCall, response: decryptedResponse);
+            }
+        }
+    }
+
+    private static IEnumerable<(Moves call, Moves response)> DecryptStrategyGuideViaCalculatedResponse(string strategyGuide)
+    {
+        if (string.IsNullOrWhiteSpace(strategyGuide) == false)
+        {
+            var encryptedPages = strategyGuide.Split(Environment.NewLine, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+            foreach (var encryptedPage in encryptedPages)
+            {
+                var encryptedPageContent = encryptedPage.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+                var encryptedCall = encryptedPageContent[0].ToUpperInvariant();
+                var encryptedResult = encryptedPageContent[1].ToUpperInvariant();
+                var decryptedCall = encryptedCall switch
+                {
+                    "A" => Moves.Rock,
+                    "B" => Moves.Paper,
+                    "C" => Moves.Scissors,
+                    _ => throw new Exception($"Unable to decrypt call: '{encryptedCall}'"),
+                };
+                var response = encryptedResult switch
+                {
+                    "X" => Moves.Rock,
+                    "Y" => Moves.Paper,
+                    "Z" => Moves.Scissors,
+                    _ => throw new Exception($"Unable to decrypt response: '{encryptedResult}'"),
+                };
+                if (decryptedCall == Moves.Rock)
+                {
+                    if (encryptedResult == "X")
+                    {
+                        yield return (call: decryptedCall, response: Moves.Scissors); // lose
+                        continue;
+                    }
+                    else if (encryptedResult == "Y")
+                    {
+                        yield return (call: decryptedCall, response: Moves.Rock); // draw
+                        continue;
+                    }
+                    else if (encryptedResult == "Z")
+                    {
+                        yield return (call: decryptedCall, response: Moves.Paper); // win
+                        continue;
+                    }
+                }
+                else if (decryptedCall == Moves.Paper)
+                {
+                    if (encryptedResult == "X")
+                    {
+                        yield return (call: decryptedCall, response: Moves.Rock); // lose
+                        continue;
+                    }
+                    else if (encryptedResult == "Y")
+                    {
+                        yield return (call: decryptedCall, response: Moves.Paper); // draw
+                        continue;
+                    }
+                    else if (encryptedResult == "Z")
+                    {
+                        yield return (call: decryptedCall, response: Moves.Scissors); // win
+                        continue;
+                    }
+                }
+                else if (decryptedCall == Moves.Scissors)
+                {
+                    if (encryptedResult == "X")
+                    {
+                        yield return (call: decryptedCall, response: Moves.Paper); // lose
+                        continue;
+                    }
+                    else if (encryptedResult == "Y")
+                    {
+                        yield return (call: decryptedCall, response: Moves.Scissors); // draw
+                        continue;
+                    }
+                    else if (encryptedResult == "Z")
+                    {
+                        yield return (call: decryptedCall, response: Moves.Rock); // win
+                        continue;
+                    }
+                }
+                throw new Exception("123"); // too lazy to make a good msg
             }
         }
     }
