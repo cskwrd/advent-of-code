b74d66c22bad01b42c0f07f7e0b967e1be374b1c
cskwrd <cskwrd@users.noreply.github.com>
2024-12-01 19:46:50 -0600

    Solve 2024 Day 1 Part 1

---
+++ src/AOC/TwentyFour/Day1.cs
@@ -0,0 +1,36 @@
+﻿namespace AOC.TwentyFour;
+public class Day1 : DayBase
+{
+    public override string AnswerPart1(string input)
+    {
+        var left = new List<long>();
+        var right = new List<long>();
+        var locationLines = ReadLocationIdsFromLocationList(input)
+            .Where(LineIsNotEmpty)
+            .Select(l => l.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries));
+        foreach (var locations in locationLines)
+        {
+            left.Add(long.Parse(locations[0]));
+            right.Add(long.Parse(locations[1]));
+        }
+
+        if (left.Count != right.Count)
+        {
+            throw new InvalidOperationException("Cannot operate on uneven lists.");
+        }
+
+        left.Sort();
+        right.Sort();
+
+        return left.Zip(right)
+            .Sum(locations => Math.Abs(locations.First - locations.Second))
+            .ToString();
+    }
+
+    public override string AnswerPart2(string input)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static IEnumerable<string> ReadLocationIdsFromLocationList(string input) => ConvertInputToLines(input);
+}
---
+++ tests/AOC.Tests/TwentyFour/Day_1_Tests.cs
@@ -0,0 +1,61 @@
+using AOC.Tests.Common;
+using AOC.TwentyFour;
+using FluentAssertions;
+
+namespace AOC.Tests.TwentyFour;
+public class Day_1_Tests : TestBase
+{
+    //https://adventofcode.com/2024/day/1
+    
+    const string EXAMPLE_1_INPUT = "AOC.Tests.Resources.Input.TwoThousand.TwentyFour.Day1.Example1.input.txt";
+    const string EXAMPLE_2_INPUT = "AOC.Tests.Resources.Input.TwoThousand.TwentyFour.Day1.Example2.input.txt";
+    const string PUZZLE_INPUT = "AOC.Tests.Resources.Input.TwoThousand.TwentyFour.Day1.Puzzle.input.txt";
+    
+    [Fact]
+    public async Task Part1Example()
+    {
+        var input = await ReadFromResourceAsync(EXAMPLE_1_INPUT);
+
+        var sut = new Day1();
+
+        string answer = sut.AnswerPart1(input);
+
+        answer.Should().Be("11");
+    }
+    
+    [Fact]
+    public async Task Part1()
+    {
+        var input = await ReadFromResourceAsync(PUZZLE_INPUT);
+
+        var sut = new Day1();
+
+        string answer = sut.AnswerPart1(input);
+
+        answer.Should().Be("2375403");
+    }
+    
+    // [Fact]
+    // public async Task Part2Example()
+    // {
+    //     var input = await ReadFromResourceAsync(EXAMPLE_2_INPUT);
+
+    //     var sut = new Day1();
+
+    //     string answer = sut.AnswerPart2(input);
+
+    //     answer.Should().Be("281");
+    // }
+    
+    // [Fact]
+    // public async Task Part2()
+    // {
+    //     var input = await ReadFromResourceAsync(PUZZLE_INPUT);
+
+    //     var sut = new Day1();
+
+    //     string answer = sut.AnswerPart2(input);
+
+    //     answer.Should().Be("54473");
+    // }
+}
