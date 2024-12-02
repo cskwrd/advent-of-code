2dd1e96dfdc164c9c84b58cdf8721f9009eed949
cskwrd <cskwrd@users.noreply.github.com>
2024-12-01 21:53:16 -0600

    Solve 2024 Day 1 Part 2

--- src/AOC/TwentyFour/Day1.cs
+++ src/AOC/TwentyFour/Day1.cs
@@ -29,7 +29,30 @@ public class Day1 : DayBase
 
     public override string AnswerPart2(string input)
     {
-        throw new NotImplementedException();
+        var similarities = new Dictionary<string, int>();
+        var runningTotal = new List<Func<long>>();
+        var locationLines = ReadLocationIdsFromLocationList(input)
+            .Where(LineIsNotEmpty)
+            .Select(l => l.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries));
+        foreach (var locations in locationLines)
+        {
+            var location = locations[0];
+            var otherLocation = locations[1];
+            runningTotal.Add(() => {
+                int numTimes;
+                if (similarities.TryGetValue(location, out numTimes) == false)
+                {
+                    numTimes = 0;
+                }
+                Console.WriteLine($"long.Parse({location}) * {numTimes} = {long.Parse(location) * numTimes}");
+                return long.Parse(location) * numTimes;
+            });
+            if (similarities.TryAdd(otherLocation, 1) == false)
+            {
+                similarities[otherLocation] += 1;
+            }
+        }
+        return runningTotal.Sum(rt => rt()).ToString();
     }
 
     private static IEnumerable<string> ReadLocationIdsFromLocationList(string input) => ConvertInputToLines(input);
--- tests/AOC.Tests/TwentyFour/Day_1_Tests.cs
+++ tests/AOC.Tests/TwentyFour/Day_1_Tests.cs
@@ -7,14 +7,13 @@ public class Day_1_Tests : TestBase
 {
     //https://adventofcode.com/2024/day/1
     
-    const string EXAMPLE_1_INPUT = "AOC.Tests.Resources.Input.TwoThousand.TwentyFour.Day1.Example1.input.txt";
-    const string EXAMPLE_2_INPUT = "AOC.Tests.Resources.Input.TwoThousand.TwentyFour.Day1.Example2.input.txt";
+    const string EXAMPLE_INPUT = "AOC.Tests.Resources.Input.TwoThousand.TwentyFour.Day1.Example1.input.txt";
     const string PUZZLE_INPUT = "AOC.Tests.Resources.Input.TwoThousand.TwentyFour.Day1.Puzzle.input.txt";
     
     [Fact]
     public async Task Part1Example()
     {
-        var input = await ReadFromResourceAsync(EXAMPLE_1_INPUT);
+        var input = await ReadFromResourceAsync(EXAMPLE_INPUT);
 
         var sut = new Day1();
 
@@ -35,27 +34,27 @@ public class Day_1_Tests : TestBase
         answer.Should().Be("2375403");
     }
     
-    // [Fact]
-    // public async Task Part2Example()
-    // {
-    //     var input = await ReadFromResourceAsync(EXAMPLE_2_INPUT);
+    [Fact]
+    public async Task Part2Example()
+    {
+        var input = await ReadFromResourceAsync(EXAMPLE_INPUT);
 
-    //     var sut = new Day1();
+        var sut = new Day1();
 
-    //     string answer = sut.AnswerPart2(input);
+        string answer = sut.AnswerPart2(input);
 
-    //     answer.Should().Be("281");
-    // }
+        answer.Should().Be("31");
+    }
     
-    // [Fact]
-    // public async Task Part2()
-    // {
-    //     var input = await ReadFromResourceAsync(PUZZLE_INPUT);
+    [Fact]
+    public async Task Part2()
+    {
+        var input = await ReadFromResourceAsync(PUZZLE_INPUT);
 
-    //     var sut = new Day1();
+        var sut = new Day1();
 
-    //     string answer = sut.AnswerPart2(input);
+        string answer = sut.AnswerPart2(input);
 
-    //     answer.Should().Be("54473");
-    // }
+        answer.Should().Be("23082277");
+    }
 }
